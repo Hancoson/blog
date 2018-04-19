d00cbd22de8e8b2595af7dba688b77e2ebfa02e2
Hancoson <hancoson@163.com>
2018-04-19 14:21:39 +0800

    add node request

--- demo/nodeMidd/utils/fetch.js
+++ demo/nodeMidd/utils/fetch.js
@@ -3,9 +3,24 @@
  * @Date: 2018-04-18 13:44:31 
  * @version 0.0.1 
  */
-var fetch = require('node-fetch');
-module.exports = function (url, method, options, callback, errBack) {
-  fetch(url, {
+var request = require('request');
+var errCode = request('./../config/errCode.js')
+module.exports = function (url, options, callback) {
+
+  request(url, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    if (response && response.statusCode == 200) {
+      callback(error, body)
+    }
+    else {
+      callback(error, response && errCode.response.statusCode)
+    }
+
+
+  });
+  /* fetch(url, {
     method: method,
     body: options,
     timeout: 3000,
